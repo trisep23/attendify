37379a7cf06a118bbf9c1c31ff7a9611d54adb4e
trisep <triseptiani1313@gmail.com>
2026-07-13 14:37:29 +0700

    tambah wifiii3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
                             <a
                                 href="{{ route('register') }}"
                                 class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
-                                Register
+                                Daftar
                             </a>
                         @endif
                     @endauth
