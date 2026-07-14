7a7485d0845a5a5869237edb0e534269536baa87
VelvetS2 <aboutvelvety@gmail.com>
2026-07-14 14:13:31 +0700

    feat: seed historical attendance data for three users from July 6 to July 13, 2026

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,17 +18,8 @@ class UserSeeder extends Seeder
             ]
         );
 
-        User::updateOrCreate(
+        $user1 = User::updateOrCreate(
             ['email' => 'trisep@gmail.com'],
-            [
-                'name' => 'trisep',
-                'password' => 'password',
-                'role' => 'user',
-            ]
-        );
-
-        User::updateOrCreate(
-            ['email' => 'tri@attendify.test'],
             [
                 'name' => 'Tri Septiani',
                 'password' => 'password',
@@ -36,22 +27,65 @@ class UserSeeder extends Seeder
             ]
         );
 
-        User::updateOrCreate(
-            ['email' => 'iin@attendify.test'],
+        $user2 = User::updateOrCreate(
+            ['email' => 'iin@gmail.com'],
             [
-                'name' => 'IIn Sumarni',
+                'name' => 'Iin Sumarni',
                 'password' => 'password',
                 'role' => 'user',
             ]
         );
 
-        User::updateOrCreate(
-            ['email' => 'hafiz@attendify.test'],
+        $user3 = User::updateOrCreate(
+            ['email' => 'hafiz@gmail.com'],
             [
                 'name' => 'Muhammad Hafiz Assyifa',
                 'password' => 'password',
                 'role' => 'user',
             ]
         );
+
+        // Hapus user dengan email domain lama agar bersih
+        User::whereIn('email', [
+            'tri@attendify.test',
+            'iin@attendify.test',
+            'hafiz@attendify.test'
+        ])->delete();
+
+        // Seed data absensi tanggal 6 s.d 13 Juli 2026
+        $startDate = new \DateTime('2026-07-06');
+        $endDate = new \DateTime('2026-07-13');
+        $interval = new \DateInterval('P1D');
+        $dateRange = new \DatePeriod($startDate, $interval, $endDate->modify('+1 day'));
+
+        $users = [$user1, $user2, $user3];
+
+        foreach ($users as $u) {
+            foreach ($dateRange as $date) {
+                $dayOfWeek = (int) $date->format('N'); // 1 (Senin) s.d 7 (Minggu)
+
+                // Absensi hanya di hari kerja (Senin s.d Jumat)
+                if ($dayOfWeek <= 5) {
+                    // Membuat jam masuk acak antara 07:30 s.d 08:15
+                    $checkInHour = rand(0, 1) === 0 ? '07' : '08';
+                    $checkInMin = $checkInHour === '07' ? str_pad(rand(30, 59), 2, '0', STR_PAD_LEFT) : str_pad(rand(0, 15), 2, '0', STR_PAD_LEFT);
+                    $checkInSec = str_pad(rand(0, 59), 2, '0', STR_PAD_LEFT);
+
+                    // Membuat jam pulang acak antara 17:00 s.d 17:45
+                    $checkOutHour = '17';
+                    $checkOutMin = str_pad(rand(0, 45), 2, '0', STR_PAD_LEFT);
+                    $checkOutSec = str_pad(rand(0, 59), 2, '0', STR_PAD_LEFT);
+
+                    $u->attendances()->updateOrCreate(
+                        ['date' => $date->format('Y-m-d')],
+                        [
+                            'check_in_time' => "$checkInHour:$checkInMin:$checkInSec",
+                            'check_out_time' => "$checkOutHour:$checkOutMin:$checkOutSec",
+                            'status' => 'hadir',
+                        ]
+                    );
+                }
+            }
+        }
     }
 }
